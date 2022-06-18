004252320c2df44d61151e85404e0d9f9df9ecae
Osquel <77018049+osquelsantisteban@users.noreply.github.com>
2022-06-18 19:40:13 -0400

    Ya sale la luna y el sol

--- js/light-dark-mode.js
+++ js/light-dark-mode.js
@@ -4,8 +4,10 @@ var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches) || themeToggleLightIcon==null) {  
   themeToggleLightIcon.classList.remove('hidden');
+  document.documentElement.classList.add('dark');
 } else {
     themeToggleDarkIcon.classList.remove('hidden');
+    document.documentElement.classList.add('light');
 }
 
 var themeToggleBtn = document.getElementById('theme-toggle');
@@ -38,3 +40,32 @@ themeToggleBtn.addEventListener('click', function() {
     }
     
 });
+
+
+function switchMode(){
+  alert('sal cojone');
+  // toggle icons inside button
+  themeToggleDarkIcon.classList.toggle('hidden');
+  themeToggleLightIcon.classList.toggle('hidden');
+
+  // if set via local storage previously
+  if (localStorage.getItem('color-theme')) {
+      if (localStorage.getItem('color-theme') === 'light') {
+          document.documentElement.classList.add('dark');
+          localStorage.setItem('color-theme', 'dark');
+      } else {
+          document.documentElement.classList.remove('dark');
+          localStorage.setItem('color-theme', 'light');
+      }
+
+  // if NOT set via local storage previously
+  } else {
+      if (document.documentElement.classList.contains('dark')) {
+          document.documentElement.classList.remove('dark');
+          localStorage.setItem('color-theme', 'light');
+      } else {
+          document.documentElement.classList.add('dark');
+          localStorage.setItem('color-theme', 'dark');
+      }
+  }
+}
