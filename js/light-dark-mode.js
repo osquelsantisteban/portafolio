32ee95b8fc35b81fc972caee8d106a3cde6aa20b
Osquel <77018049+osquelsantisteban@users.noreply.github.com>
2022-10-22 19:45:59 -0400

    Osquel's Portfolio

--- js/light-dark-mode.js
+++ js/light-dark-mode.js
@@ -3,10 +3,10 @@ var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    console.log('dark');
+    // console.log('dark');
     themeToggleDarkIcon.classList.remove('hidden');
 } else {
-    console.log('light');
+    // console.log('light');
     themeToggleLightIcon.classList.remove('hidden');
 }
 
